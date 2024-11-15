5222d85ce87e5246b65fcc4f40e5f842de54dac7
Raghvendra Singh <108226988+RAGHVENDRA04@users.noreply.github.com>
2024-11-15 14:25:48 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `${APIUrl}/auth/login`;
+            const url = `https://rajsinghtaskman.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
