a7efb9462429f67b53d2bd7160473f91489cde4d
Raghvendra Singh <108226988+RAGHVENDRA04@users.noreply.github.com>
2024-11-15 14:25:11 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `${APIUrl}/auth/signup`;
+            const url = `https://rajsinghtaskman.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
